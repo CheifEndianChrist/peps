f2de802a9f67c8e7d16efdbc4f9c9ffb53b03f52
CheifEndianChrist <bochaney5@yahoo.com>
2025-08-21 08:22:48 -0400

    Improve PEP index JSON test

--- pep_sphinx_extensions/tests/pep_zero_generator/test_pep_index_generator.py
+++ pep_sphinx_extensions/tests/pep_zero_generator/test_pep_index_generator.py
@@ -1,3 +1,4 @@
+import json
 from pep_sphinx_extensions.pep_zero_generator import parser, pep_index_generator
 
 from ..conftest import PEP_ROOT
@@ -8,4 +9,8 @@ def test_create_pep_json():
 
     out = pep_index_generator.create_pep_json(peps)
 
-    assert '"url": "https://peps.python.org/pep-0008/"' in out
+    data = json.loads(out)
+    data = {int(k): v for k, v in data.items()}
+
+    assert 8 in data
+    assert data[8]["url"] == "https://peps.python.org/pep-0008/"
